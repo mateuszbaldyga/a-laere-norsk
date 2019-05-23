2e0f3d21fb13832fb7f107dff78bc632a999b6e4
Mateusz Bałdyga <matbaldyga@gmail.com>
2019-05-23 21:41:59 +0200

    dictionary beautify code

--- src/dictionary/verbs.js
+++ src/dictionary/verbs.js
@@ -1,362 +1,398 @@
 const dict = [
-    [
-        {
-            'no': 'å ligge, ligger',
-            'pl': 'leżeć',
-            'id': 'verb0',
-        },
-        {
-            'no': 'å vaske vasker',
-            'pl': 'myć',
-            'id': 'verb1',
-        },
-        {
-            'no': 'å vaske opp',
-            'pl': 'zmywać',
-            'id': 'verb2',
-        },
-        {
-            'no': 'å sitte, sitter',
-            'pl': 'siedzieć',
-            'id': 'verb3',
-        },
-        {
-            'no': 'å se, ser',
-            'pl': 'patrzyć',
-            'id': 'verb4',
-        },
-        {
-            'no': 'å tenke, tenker',
-            'pl': 'myśleć',
-            'id': 'verb5',
-        },
-        {
-            'no': 'å tenke på',
-            'pl': 'myśleć o',
-            'id': 'verb6',
-        },
-        {
-            'no': 'å sykle, sykler',
-            'pl': 'jechać na rowerze',
-            'id': 'verb7',
-        },
-        {
-            'no': 'å sende, sender',
-            'pl': 'wysyłać',
-            'id': 'verb8',
-        },
-        {
-            'no': 'å finne, finner',
-            'pl': 'znajdować',
-            'id': 'verb9',
-        },
-        {
-            'no': 'å fortelle om, forteller om',
-            'pl': 'opowiadać o',
-            'id': 'verb10',
-        },
-        {
-            'no': 'å lete etter, leter etter',
-            'pl': 'szukać',
-            'id': 'verb11',
-        },
-        {
-            'no': 'å lengte etter, lengter etter',
-            'pl': 'tęsknić',
-            'id': 'verb12',
-        },
-        {
-            'no': 'å treffe, treffer',
-            'pl': 'spotykać',
-            'id': 'verb13',
-        },
-        {
-            'no': 'å bære, bærer',
-            'pl': 'nosić',
-            'id': 'verb14',
-        },
+    [{
+        'no': 'å ligge, ligger',
+        'pl': 'leżeć',
+        'id': 'verb0',
+    },
+    {
+        'no': 'å vaske vasker',
+        'pl': 'myć',
+        'id': 'verb1',
+    },
+    {
+        'no': 'å vaske opp',
+        'pl': 'zmywać',
+        'id': 'verb2',
+    },
+    {
+        'no': 'å sitte, sitter',
+        'pl': 'siedzieć',
+        'id': 'verb3',
+    },
+    {
+        'no': 'å se, ser',
+        'pl': 'patrzyć',
+        'id': 'verb4',
+    },
+    {
+        'no': 'å tenke, tenker',
+        'pl': 'myśleć',
+        'id': 'verb5',
+    },
+    {
+        'no': 'å tenke på',
+        'pl': 'myśleć o',
+        'id': 'verb6',
+    },
+    {
+        'no': 'å sykle, sykler',
+        'pl': 'jechać na rowerze',
+        'id': 'verb7',
+    },
+    {
+        'no': 'å sende, sender',
+        'pl': 'wysyłać',
+        'id': 'verb8',
+    },
+    {
+        'no': 'å finne, finner',
+        'pl': 'znajdować',
+        'id': 'verb9',
+    },
+    {
+        'no': 'å fortelle om, forteller om',
+        'pl': 'opowiadać o',
+        'id': 'verb10',
+    },
+    {
+        'no': 'å lete etter, leter etter',
+        'pl': 'szukać',
+        'id': 'verb11',
+    },
+    {
+        'no': 'å lengte etter, lengter etter',
+        'pl': 'tęsknić',
+        'id': 'verb12',
+    },
+    {
+        'no': 'å treffe, treffer',
+        'pl': 'spotykać',
+        'id': 'verb13',
+    },
+    {
+        'no': 'å bære, bærer',
+        'pl': 'nosić',
+        'id': 'verb14',
+    },
     ],
-    [
-        {
-            'no': 'å stave, staver',
-            'pl': 'literować',
-            'id': 'verb15',
-        },
-        {
-            'no': 'å lære, lærer',
-            'pl': 'uczyć się',
-            'id': 'verb16',
-        },
-        {
-            'no': 'å spørre, spør',
-            'pl': 'pytać',
-            'id': 'verb17',
-        },
-        {
-            'no': 'å prøve, prøver',
-            'pl': 'próbować',
-            'id': 'verb18',
-        },
-        {
-            'no': 'å si, sier',
-            'pl': 'mówić, powiedzieć',
-            'id': 'verb19',
-        },
-        {
-            'no': 'å gå, går',
-            'pl': 'iść',
-            'id': 'verb20',
-        },
-        {
-            'no': 'å like, liker',
-            'pl': 'lubić',
-            'id': 'verb21',
-        },
+    [{
+        'no': 'å stave, staver',
+        'pl': 'literować',
+        'id': 'verb15',
+    },
+    {
+        'no': 'å lære, lærer',
+        'pl': 'uczyć się',
+        'id': 'verb16',
+    },
+    {
+        'no': 'å spørre, spør',
+        'pl': 'pytać',
+        'id': 'verb17',
+    },
+    {
+        'no': 'å prøve, prøver',
+        'pl': 'próbować',
+        'id': 'verb18',
+    },
+    {
+        'no': 'å si, sier',
+        'pl': 'mówić, powiedzieć',
+        'id': 'verb19',
+    },
+    {
+        'no': 'å gå, går',
+        'pl': 'iść',
+        'id': 'verb20',
+    },
+    {
+        'no': 'å like, liker',
+        'pl': 'lubić',
+        'id': 'verb21',
+    },
     ],
-    [
-        {
-            'no': 'å besøke',
-            'pl': 'odwiedzać',
-            'id': 'verb22',
-        },
-        {
-            'no': 'å skjønne',
-            'pl': 'rozumieć',
-            'id': 'verb23',
-        },
-        {
-            'no': 'å bruke',
-            'pl': 'używać',
-            'id': 'verb24',
-        },
-        {
-            'no': 'å ligge',
-            'pl': 'leżeć',
-            'id': 'verb25',
-        },
-        {
-            'no': 'å handle',
-            'pl': 'robić zakupy',
-            'id': 'verb26',
-        },
-        {
-            'no': 'å kjøpe',
-            'pl': 'kupować',
-            'id': 'verb27',
-        },
-        {
-            'no': 'å vente',
-            'pl': 'czekać',
-            'id': 'verb28',
-        },
-        {
-            'no': 'å trenge',
-            'pl': 'potrzebować',
-            'id': 'verb29',
-        },
-        {
-            'no': 'å smile',
-            'pl': 'uśmiechać się',
-            'id': 'verb30',
-        },
-        {
-            'no': 'å betale',
-            'pl': 'płacić',
-            'id': 'verb31',
-        },
+    [{
+        'no': 'å besøke',
+        'pl': 'odwiedzać',
+        'id': 'verb22',
+    },
+    {
+        'no': 'å skjønne',
+        'pl': 'rozumieć',
+        'id': 'verb23',
+    },
+    {
+        'no': 'å bruke',
+        'pl': 'używać',
+        'id': 'verb24',
+    },
+    {
+        'no': 'å ligge',
+        'pl': 'leżeć',
+        'id': 'verb25',
+    },
+    {
+        'no': 'å handle',
+        'pl': 'robić zakupy',
+        'id': 'verb26',
+    },
+    {
+        'no': 'å kjøpe',
+        'pl': 'kupować',
+        'id': 'verb27',
+    },
+    {
+        'no': 'å vente',
+        'pl': 'czekać',
+        'id': 'verb28',
+    },
+    {
+        'no': 'å trenge',
+        'pl': 'potrzebować',
+        'id': 'verb29',
+    },
+    {
+        'no': 'å smile',
+        'pl': 'uśmiechać się',
+        'id': 'verb30',
+    },
+    {
+        'no': 'å betale',
+        'pl': 'płacić',
+        'id': 'verb31',
+    },
     ],
-    [
-        {
-            'no': 'å stå opp, står op',
-            'pl': 'wstawać',
-            'id': 'verb33',
-        },
-        {
-            'no': 'å gjøre, gjør',
-            'pl': 'robić',
-            'id': 'verb34',
-        },
-        {
-            'no': 'å stå, står',
-            'pl': 'stać',
-            'id': 'verb35',
-        },
-        {
-            'no': 'å begynne, begynner',
-            'pl': 'zaczynać',
-            'id': 'verb36',
-        },
-        {
-            'no': 'å arbeide, arbeider',
-            'pl': 'pracować',
-            'id': 'verb37',
-        },
-        {
-            'no': 'å slutte, slutter',
-            'pl': 'kończyć',
-            'id': 'verb38',
-        },
-        {
-            'no': 'å vekke, vekker',
-            'pl': 'budzić',
-            'id': 'verb39',
-        },
-        {
-            'no': 'å lage, lager',
-            'pl': 'przygotowywać',
-            'id': 'verb40',
-        },
-        {
-            'no': 'å ta, tar',
-            'pl': 'brać',
-            'id': 'verb41',
-        },
-        {
-            'no': 'å sove, sover',
-            'pl': 'spać',
-            'id': 'verb42',
-        },
-        {
-            'no': 'å fly, flyr',
-            'pl': 'latać',
-            'id': 'verb43',
-        },
-        {
-            'no': 'å leke, leker',
-            'pl': 'bawić się',
-            'id': 'verb44',
-        },
-        {
-            'no': 'å komme, kommer',
-            'pl': 'przychodzić, przyjeżdżać',
-            'id': 'verb45',
-        },
-        {
-            'no': 'å gjenta, gjentar',
-            'pl': 'powtórzyć',
-            'id': 'verb46',
-        },
-        {
-            'no': 'å pleie, pleier',
-            'pl': 'mieć w zwyczaju',
-            'id': 'verb47',
-        },
+    [{
+        'no': 'å stå opp, står op',
+        'pl': 'wstawać',
+        'id': 'verb33',
+    },
+    {
+        'no': 'å gjøre, gjør',
+        'pl': 'robić',
+        'id': 'verb34',
+    },
+    {
+        'no': 'å stå, står',
+        'pl': 'stać',
+        'id': 'verb35',
+    },
+    {
+        'no': 'å begynne, begynner',
+        'pl': 'zaczynać',
+        'id': 'verb36',
+    },
+    {
+        'no': 'å arbeide, arbeider',
+        'pl': 'pracować',
+        'id': 'verb37',
+    },
+    {
+        'no': 'å slutte, slutter',
+        'pl': 'kończyć',
+        'id': 'verb38',
+    },
+    {
+        'no': 'å vekke, vekker',
+        'pl': 'budzić',
+        'id': 'verb39',
+    },
+    {
+        'no': 'å lage, lager',
+        'pl': 'przygotowywać',
+        'id': 'verb40',
+    },
+    {
+        'no': 'å ta, tar',
+        'pl': 'brać',
+        'id': 'verb41',
+    },
+    {
+        'no': 'å sove, sover',
+        'pl': 'spać',
+        'id': 'verb42',
+    },
+    {
+        'no': 'å fly, flyr',
+        'pl': 'latać',
+        'id': 'verb43',
+    },
+    {
+        'no': 'å leke, leker',
+        'pl': 'bawić się',
+        'id': 'verb44',
+    },
+    {
+        'no': 'å komme, kommer',
+        'pl': 'przychodzić, przyjeżdżać',
+        'id': 'verb45',
+    },
+    {
+        'no': 'å gjenta, gjentar',
+        'pl': 'powtórzyć',
+        'id': 'verb46',
+    },
+    {
+        'no': 'å pleie, pleier',
+        'pl': 'mieć w zwyczaju',
+        'id': 'verb47',
+    },
     ],
-    [
-        {
-            'no': 'å bake baker',
-            'pl': 'piec',
-            'id': 'verb48',
-        },
-        {
-            'no': 'å steke steker',
-            'pl': 'piec, smarzyć',
-            'id': 'verb49',
-        },
-        {
-            'no': 'å smake smaker',
-            'pl': 'próbować, smakować',
-            'id': 'verb50',
-        },
-        {
-            'no': 'å hjelpe hjelper',
-            'pl': 'pomagać',
-            'id': 'verb51',
-        },
-        {
-            'no': 'å rydde rydder',
-            'pl': 'sprzątać',
-            'id': 'verb52',
-        },
-        {
-            'no': 'å pusse pusser',
-            'pl': 'czyścić',
-            'id': 'verb53',
-        },
-        {
-            'no': 'å dra drar',
-            'pl': 'ciągnąć, przeciągnąć, jechać',
-            'id': 'verb54',
-        },
-        {
-            'no': 'å hente henter',
-            'pl': 'przynosić, przywozić, przyprowadzać',
-            'id': 'verb55',
-        },
-        {
-            'no': 'å kjøre kjører',
-            'pl': 'jechać, prowadzić samochód',
-            'id': 'verb56',
-        },
-        {
-            'no': 'å dekke dekker',
-            'pl': 'nakryć, przykryć',
-            'id': 'verb57',
-        },
-        {
-            'no': 'å gi gir',
-            'pl': 'dawać, podawać',
-            'id': 'verb58',
-        },
-        {
-            'no': 'å vite vet',
-            'pl': 'wiedzieć',
-            'id': 'verb59',
-        },
-        {
-            'no': 'å bestille bestiller',
-            'pl': 'zamawiać',
-            'id': 'verb60',
-        },
+    [{
+        'no': 'å bake baker',
+        'pl': 'piec',
+        'id': 'verb48',
+    },
+    {
+        'no': 'å steke steker',
+        'pl': 'piec, smarzyć',
+        'id': 'verb49',
+    },
+    {
+        'no': 'å smake smaker',
+        'pl': 'próbować, smakować',
+        'id': 'verb50',
+    },
+    {
+        'no': 'å hjelpe hjelper',
+        'pl': 'pomagać',
+        'id': 'verb51',
+    },
+    {
+        'no': 'å rydde rydder',
+        'pl': 'sprzątać',
+        'id': 'verb52',
+    },
+    {
+        'no': 'å pusse pusser',
+        'pl': 'czyścić',
+        'id': 'verb53',
+    },
+    {
+        'no': 'å dra drar',
+        'pl': 'ciągnąć, przeciągnąć, jechać',
+        'id': 'verb54',
+    },
+    {
+        'no': 'å hente henter',
+        'pl': 'przynosić, przywozić, przyprowadzać',
+        'id': 'verb55',
+    },
+    {
+        'no': 'å kjøre kjører',
+        'pl': 'jechać, prowadzić samochód',
+        'id': 'verb56',
+    },
+    {
+        'no': 'å dekke dekker',
+        'pl': 'nakryć, przykryć',
+        'id': 'verb57',
+    },
+    {
+        'no': 'å gi gir',
+        'pl': 'dawać, podawać',
+        'id': 'verb58',
+    },
+    {
+        'no': 'å vite vet',
+        'pl': 'wiedzieć',
+        'id': 'verb59',
+    },
+    {
+        'no': 'å bestille bestiller',
+        'pl': 'zamawiać',
+        'id': 'verb60',
+    },
     ],
-    [
-        {
-            'no': 'å hjelpe, hjelper',
-            'pl': 'pomagać',
-            'id': 'verb61',
-        },
-        {
-            'no': 'å veie, veier',
-            'pl': 'ważyć',
-            'id': 'verb62',
-        },
-        {
-            'no': 'å veksle, veksler (exchange)',
-            'pl': 'wymieniać',
-            'id': 'verb63',
-        },
-        {
-            'no': 'å overføre, overfører',
-            'pl': 'przelać',
-            'id': 'verb64',
-        },
-        {
-            'no': 'å fylle, fyller',
-            'pl': 'wypełniać',
-            'id': 'verb65',
-        },
-        {
-            'no': 'å gi, gir',
-            'pl': 'dawać, podawać',
-            'id': 'verb66',
-        },
-        {
-            'no': 'å låne, låner',
-            'pl': 'pożyczyć, wypożyczyć',
-            'id': 'verb67',
-        },
-        {
-            'no': 'å bli, blir',
-            'pl': 'zostać, pozostać',
-            'id': 'verb68',
-        },
-        {
-            'no': 'å få, får',
-            'pl': 'otrzymać',
-            'id': 'verb69',
-        },
+    [{
+        'no': 'å hjelpe, hjelper',
+        'pl': 'pomagać',
+        'id': 'verb61',
+    },
+    {
+        'no': 'å veie, veier',
+        'pl': 'ważyć',
+        'id': 'verb62',
+    },
+    {
+        'no': 'å veksle, veksler (exchange)',
+        'pl': 'wymieniać',
+        'id': 'verb63',
+    },
+    {
+        'no': 'å overføre, overfører',
+        'pl': 'przelać',
+        'id': 'verb64',
+    },
+    {
+        'no': 'å fylle, fyller',
+        'pl': 'wypełniać',
+        'id': 'verb65',
+    },
+    {
+        'no': 'å gi, gir',
+        'pl': 'dawać, podawać',
+        'id': 'verb66',
+    },
+    {
+        'no': 'å låne, låner',
+        'pl': 'pożyczyć, wypożyczyć',
+        'id': 'verb67',
+    },
+    {
+        'no': 'å bli, blir',
+        'pl': 'zostać, pozostać',
+        'id': 'verb68',
+    },
+    {
+        'no': 'å få, får',
+        'pl': 'otrzymać',
+        'id': 'verb69',
+    },
     ],
-    [{ 'no': 'å passe seg', 'pl': 'uważać się' }, { 'no': 'å vaske seg', 'pl': 'myć się' }, { 'no': 'å gifte seg', 'pl': 'żenić się' }, { 'no': 'å legge seg', 'pl': 'kłaść się' }, { 'no': 'å glede seg', 'pl': 'cieszyć się' }, { 'no': 'å sette seg', 'pl': 'siadać' }, { 'no': 'å ønske seg', 'pl': 'życzyć' }, { 'no': 'å hvile seg', 'pl': 'odpoczywać' }, { 'no': 'å sole seg', 'pl': 'opalać się' }, { 'no': 'å slanke seg /szlanke/', 'pl': 'odchudzać się' }, { 'no': 'å skynde seg /szyne/', 'pl': 'śpieszyć się' }, { 'no': 'å lønne seg', 'pl': 'opłacać się' }, { 'no': 'å kle på seg', 'pl': 'ubierać się' }, { 'no': 'å kle av seg', 'pl': 'rozbierać się' }],
+    [{
+        'no': 'å passe seg',
+        'pl': 'uważać się',
+    }, {
+        'no': 'å vaske seg',
+        'pl': 'myć się',
+    }, {
+        'no': 'å gifte seg',
+        'pl': 'żenić się',
+    }, {
+        'no': 'å legge seg',
+        'pl': 'kłaść się',
+    }, {
+        'no': 'å glede seg',
+        'pl': 'cieszyć się',
+    }, {
+        'no': 'å sette seg',
+        'pl': 'siadać',
+    }, {
+        'no': 'å ønske seg',
+        'pl': 'życzyć',
+    }, {
+        'no': 'å hvile seg',
+        'pl': 'odpoczywać',
+    }, {
+        'no': 'å sole seg',
+        'pl': 'opalać się',
+    }, {
+        'no': 'å slanke seg /szlanke/',
+        'pl': 'odchudzać się',
+    }, {
+        'no': 'å skynde seg /szyne/',
+        'pl': 'śpieszyć się',
+    }, {
+        'no': 'å lønne seg',
+        'pl': 'opłacać się',
+    }, {
+        'no': 'å kle på seg',
+        'pl': 'ubierać się',
+    }, {
+        'no': 'å kle av seg',
+        'pl': 'rozbierać się',
+    }],
 ]
 
 export default dict
