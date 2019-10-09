493ea651403de32ac9ad1f6aee6efc305ce7bbdc
Mateusz Bałdyga <matbaldyga@gmail.com>
2019-10-09 20:18:34 +0200

    b97279b

---
+++ precache-manifest.81768299ad35d0a4e79d76ed95565302.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "75b12ffb1c5d99a3405f",
+    "url": "/a-laere-norsk/css/mastered-flashcards.6d67e3c6.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/a-laere-norsk/robots.txt"
+  },
+  {
+    "revision": "75b12ffb1c5d99a3405f",
+    "url": "/a-laere-norsk/js/mastered-flashcards.95edcc04.js"
+  },
+  {
+    "revision": "4408ecc70bf8ef4365b0",
+    "url": "/a-laere-norsk/js/chunk-vendors.8bc14321.js"
+  },
+  {
+    "revision": "bcab5847c5dd626accae",
+    "url": "/a-laere-norsk/js/create-dict.e09fbe17.js"
+  },
+  {
+    "revision": "a1e730385eee3c02d2f5",
+    "url": "/a-laere-norsk/js/search.a9d290fc.js"
+  },
+  {
+    "revision": "869c56e2523bfd04f11d",
+    "url": "/a-laere-norsk/js/flashcards.0c4b9979.js"
+  },
+  {
+    "revision": "cac4a789e48c73fba340",
+    "url": "/a-laere-norsk/js/lessons.c71cce84.js"
+  },
+  {
+    "revision": "c003b57a2498ce0f0391",
+    "url": "/a-laere-norsk/js/hard-to-remember.67ce1e46.js"
+  },
+  {
+    "revision": "edf935bf5bb2159a87d28cf9372f4221",
+    "url": "/a-laere-norsk/img/ic_launcher/res/mipmap-hdpi/ic_launcher.png"
+  },
+  {
+    "revision": "cac4a789e48c73fba340",
+    "url": "/a-laere-norsk/css/lessons.660a8232.css"
+  },
+  {
+    "revision": "af90522d0438efbd9700",
+    "url": "/a-laere-norsk/js/app.2ec31731.js"
+  },
+  {
+    "revision": "237b2cf479cb33994119a21efe6e4a82",
+    "url": "/a-laere-norsk/img/ic_launcher/web_hi_res_512.png"
+  },
+  {
+    "revision": "a1e730385eee3c02d2f5",
+    "url": "/a-laere-norsk/css/search.bc9696e7.css"
+  },
+  {
+    "revision": "8d4da0603b25053b711642e603da6175",
+    "url": "/a-laere-norsk/img/ic_launcher/res/mipmap-xxxhdpi/ic_launcher.png"
+  },
+  {
+    "revision": "bd32d72f1753f15d60ec6ccd6e886ae3",
+    "url": "/a-laere-norsk/index.html"
+  },
+  {
+    "revision": "9cf0df468923020fd35eb80790c5dedf",
+    "url": "/a-laere-norsk/img/ic_launcher/res/mipmap-xxhdpi/ic_launcher.png"
+  },
+  {
+    "revision": "2c40026ee53a04f1aa146d99fb9bcf7b",
+    "url": "/a-laere-norsk/img/ic_launcher/res/mipmap-xhdpi/ic_launcher.png"
+  },
+  {
+    "revision": "5f61b71a20ad49e7e2b97ba6a13ced09",
+    "url": "/a-laere-norsk/img/ic_launcher/res/mipmap-mdpi/ic_launcher.png"
+  },
+  {
+    "revision": "c003b57a2498ce0f0391",
+    "url": "/a-laere-norsk/css/hard-to-remember.fca2a103.css"
+  },
+  {
+    "revision": "869c56e2523bfd04f11d",
+    "url": "/a-laere-norsk/css/flashcards.d5c237a1.css"
+  },
+  {
+    "revision": "bcab5847c5dd626accae",
+    "url": "/a-laere-norsk/css/create-dict.645e1ace.css"
+  },
+  {
+    "revision": "af90522d0438efbd9700",
+    "url": "/a-laere-norsk/css/app.248dec71.css"
+  }
+];
